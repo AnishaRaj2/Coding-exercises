de3fe4e2ec642d46238bea9d9b7360eef9adc1b5
AnishaRaj2 <mail.anisha.raj@gmail.com>
2022-10-25 14:41:13 +0300

    Define JS function bingo

--- Index.js
+++ Index.js
@@ -4,5 +4,16 @@ function Printstr (a) {
     return str;
 }
 
+function Bingo (a) {
+    if (a>5){
+        return((console.log("Bingo!")));
+    } else {
+        return((console.log("Try next time.")));
+        }
+}
+
 Printstr ("Hello");
 Printstr ("How are you?");
+Bingo (2);
+Bingo (5);
+Bingo (7);
